52c7deb852df4d6f9ccf3bce5dc39bf389406550
¨Fernando¨ <tec_gomez@yahoo.com.ar>
2024-06-08 09:26:33 -0300

    ´clase-10´

---
+++ Clase-10/script.js
@@ -0,0 +1,60 @@
+/* javascript es un lenguaje interpretado */
+
+
+alert('hola mundo')
+
+
+// Tipos de datos
+
+
+/* 
+PRIMITIVOS:
+string => para representar texto
+´hola´, ´pepe´,"hola mundo", '3'
+
+bollean=> para representar estados booleanos
+treu false
+
+number=> para representar datos numericos
+1  -1  1.1  NaN  Infinity
+
+null=> para representar nulidad
+null
+
+
+undefined=> para representar el estado indefinido
+undefined
+ */
+
+
+
+// Operadores aritmeticos
+
+/* 
++ => concatenacio
+
+
++ => suma
+- => resta
+* => multiplicacion
+/ => division
+% => resto de la division
+
+*/
+
+// Comparadores SIEMPRE DEVUELVEN BOLLEAN
+
+/*
+
+== igualdad
+=== igual de valor y tipo de dato
+!=
+!==
+>
+<
+>=
+<=
+
+Excepciones:}
+null solo es igual a null o undefined
+NaN no es igual a NINGUnN OTRO DATO, en caso de  querer comprar con NaN ussar la funcion isNaN()
